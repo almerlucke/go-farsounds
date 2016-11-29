ef33b0eedd243780b47aea7488ee7a2515b978a3
Almer Lucke <almer@aliensareamongus.com>
2016-11-29 13:36:59 +0100

    working on osc plus example

--- example.go
+++ example.go
@@ -12,30 +12,28 @@ import (
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	samplerate := 44100.0
+	samplerate := 44100.0 * 4
 
 	osc1 := farsounds.NewOsc(farsounds.SineTable, 0, 100.0/samplerate, 1)
-	osc2 := farsounds.NewOsc(farsounds.SineTable, 0, 210.0/samplerate, 0.6)
+	osc2 := farsounds.NewOsc(farsounds.SineTable, 0, 201.0/samplerate, 0.6)
 	osc3 := farsounds.NewOsc(farsounds.SineTable, 0, 430.0/samplerate, 0.4)
-	osc4 := farsounds.NewOsc(farsounds.SineTable, 0, 880.0/samplerate, 0.2)
+	osc4 := farsounds.NewOsc(farsounds.SineTable, 0, 510.0/samplerate, 0.2)
 
 	outputPath := "/users/almerlucke/Desktop/output"
 
-	writer, err := soundwriter.OpenSoundWriter(outputPath, 1, int32(44100), true)
+	writer, err := soundwriter.OpenSoundWriter(outputPath, 1, int32(samplerate), true)
 	if err != nil {
 		fmt.Printf("normalize err: %v\n", err)
 		return
 	}
 
-	numSamples := 88200 * int64(writer.Channels)
+	numSamples := int64(int64(samplerate) * 2 * int64(writer.Channels))
 	samples := make([]float64, numSamples)
 
 	for index := range samples {
 		newSample := osc1.Next(0) + osc2.Next(0) + osc3.Next(0) + osc4.Next(0)
 		samples[index] = newSample
 	}
-
-	fmt.Printf("num samples %d\n", numSamples)
 
 	err = writer.WriteSamples(samples[:])
 	if err != nil {
