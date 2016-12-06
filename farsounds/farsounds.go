6f2e81b773eaffd434eb4a08fc4a4a029da94cb2
Almer Lucke <almer@aliensareamongus.com>
2016-12-06 10:12:00 +0100

    abstract soundfile generation

--- farsounds/farsounds.go
+++ farsounds/farsounds.go
@@ -1,6 +1,11 @@
 package farsounds
 
-import "aliensareamongus.com/careibu/utils/jsonx"
+import (
+	"errors"
+
+	"aliensareamongus.com/careibu/utils/jsonx"
+	"github.com/almerlucke/go-farsounds/farsounds/io"
+)
 
 // ScriptMainDescriptor describes main script content
 type ScriptMainDescriptor struct {
@@ -33,4 +38,71 @@ func LoadMainScript(filePath string) (*Patch, error) {
 	patch := _patch.(*Patch)
 
 	return patch, nil
+}
+
+// SoundFileFromPatch generate soundfile from patch
+func SoundFileFromPatch(patch *Patch, soundFilePath string, numSeconds float64) error {
+	// Store in local vars
+	sr := patch.GetSampleRate()
+	buflen := patch.GetBufferLength()
+	numChannels := int32(len(patch.GetOutlets()))
+
+	// Sanity check on numChannels
+	if numChannels < 1 || numChannels > 2 {
+		return errors.New("Main patch must have one or two outputs")
+	}
+
+	// Open sound writer
+	writer, err := io.OpenSoundWriter(soundFilePath, numChannels, int32(sr), true)
+	if err != nil {
+		return err
+	}
+
+	// Always clean up writer
+	defer writer.Close()
+
+	// Prepare DSP loop
+	timestamp := int64(0)
+	numCycles := int64(((numSeconds * sr) / float64(buflen)) + 0.5)
+	sampleBuffer := make([]float64, numChannels*buflen)
+
+	// Generate samples for N cycles
+	for i := int64(0); i < numCycles; i++ {
+		patch.PrepareDSP()
+		patch.DSP(timestamp)
+
+		// Interleave channels
+		for j := int32(0); j < buflen; j++ {
+			for c := int32(0); c < numChannels; c++ {
+				sampleBuffer[j*numChannels+c] = patch.Outlets[c].Buffer[j]
+			}
+		}
+
+		// Write samples
+		err = writer.WriteSamples(sampleBuffer)
+		if err != nil {
+			return err
+		}
+
+		// Increase timestamp
+		timestamp += int64(buflen)
+	}
+
+	// No errors
+	return nil
+}
+
+// SoundFileFromScript load script and generate soundfile
+func SoundFileFromScript(scriptPath string, soundFilePath string, numSeconds float64) error {
+	// Load main script with sr, buflen and main patch
+	patch, err := LoadMainScript(scriptPath)
+	if err != nil {
+		return err
+	}
+
+	// Always clean up patch
+	defer patch.Cleanup()
+
+	// Generate sound file from patch output
+	return SoundFileFromPatch(patch, soundFilePath, numSeconds)
 }
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/almerlucke/go-farsounds/farsounds"
+	"github.com/almerlucke/go-farsounds/farsounds/components"
+	"github.com/almerlucke/go-farsounds/farsounds/tables"
+)
+
+func setup() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	farsounds.Registry.RegisterWaveTable("sine", tables.SineTable)
+
+	farsounds.Registry.RegisterModuleFactory("osc", components.OscModuleFactory)
+	farsounds.Registry.RegisterModuleFactory("square", components.SquareModuleFactory)
+	farsounds.Registry.RegisterModuleFactory("adsr", components.ADSRModuleFactory)
+	farsounds.Registry.RegisterModuleFactory("patch", farsounds.PatchFactory)
+}
+
+func main() {
+	setup()
+
+	err := farsounds.SoundFileFromScript(
+		"exampleScripts/stereoPatch.json",
+		"/users/almerlucke/Desktop/output",
+		4.0,
+	)
+
+	if err != nil {
+		fmt.Printf("err %v\n", err)
+	}
+}
