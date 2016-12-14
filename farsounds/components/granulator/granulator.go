e45ebe07870e6b963265c5b901046804e743e631
Almer Lucke <almer@aliensareamongus.com>
2016-12-14 23:20:55 +0100

    working on granulator

--- farsounds/components/granulator/granulator.go
+++ farsounds/components/granulator/granulator.go
@@ -1,6 +1,12 @@
 package granulator
 
-import "container/list"
+import (
+	"container/list"
+	"math/rand"
+
+	"github.com/almerlucke/go-farsounds/farsounds"
+	"github.com/almerlucke/go-farsounds/farsounds/components"
+)
 
 /*
 	Interfaces
@@ -14,17 +20,17 @@ type Grain interface {
 
 // TickGenerator generates ticks
 type TickGenerator interface {
-	GetTick(timestamp int64) bool
+	GenerateTick(timestamp int64) bool
 }
 
 // DurationGenerator generates duration
 type DurationGenerator interface {
-	GetDuration(timestamp int64) float64
+	GenerateDuration(timestamp int64) float64
 }
 
 // ParameterGenerator generates parameter
 type ParameterGenerator interface {
-	GetParameters(timestamp int64) interface{}
+	GenerateParameters(timestamp int64) interface{}
 }
 
 // GrainFactory generates grains
@@ -53,6 +59,25 @@ type Granulator struct {
 	UsedGrains         *list.List
 }
 
+// NewGranulator creation
+func NewGranulator(
+	tickGenerator TickGenerator,
+	durationGenerator DurationGenerator,
+	parameterGenerator ParameterGenerator,
+	grainFactory GrainFactory) *Granulator {
+
+	granulator := new(Granulator)
+
+	granulator.TickGenerator = tickGenerator
+	granulator.DurationGenerator = durationGenerator
+	granulator.ParameterGenerator = parameterGenerator
+	granulator.GrainFactory = grainFactory
+	granulator.FreeGrains = list.New()
+	granulator.UsedGrains = list.New()
+
+	return granulator
+}
+
 // NumChannels is an indication to module using granulator how many
 // channels are used, can be 1 or 2
 func (granulator *Granulator) NumChannels() int {
@@ -78,13 +103,13 @@ func (granulator *Granulator) getFreeGrainVoice() *grainVoice {
 // Process granulator
 func (granulator *Granulator) Process(timestamp int64, sr float64) (float64, float64) {
 	// First try to get tick
-	tick := granulator.TickGenerator.GetTick(timestamp)
+	tick := granulator.TickGenerator.GenerateTick(timestamp)
 
 	// If tick then schedule a new voice
 	if tick {
 		grainVoice := granulator.getFreeGrainVoice()
-		duration := granulator.DurationGenerator.GetDuration(timestamp)
-		parameters := granulator.ParameterGenerator.GetParameters(timestamp)
+		duration := granulator.DurationGenerator.GenerateDuration(timestamp)
+		parameters := granulator.ParameterGenerator.GenerateParameters(timestamp)
 		grainVoice.grain.Initialize(duration, sr, parameters)
 		grainVoice.sampsToGo = int64(duration * sr)
 	}
@@ -113,3 +138,115 @@ func (granulator *Granulator) Process(timestamp int64, sr float64) (float64, flo
 /*
 	Granulator module
 */
+
+// GranulatorModule granulator module
+type GranulatorModule struct {
+	// Base module
+	*farsounds.BaseModule
+
+	// Granulator
+	granulator *Granulator
+}
+
+// NewGranulatorModule creates a new granulator module
+func NewGranulatorModule(granulator *Granulator, buflen int32, sr float64) *GranulatorModule {
+	granulatorModule := new(GranulatorModule)
+	granulatorModule.BaseModule = farsounds.NewBaseModule(0, granulator.NumChannels(), buflen, sr)
+	granulatorModule.Parent = granulatorModule
+	granulatorModule.granulator = granulator
+	return granulatorModule
+}
+
+// DSP for granulator module
+func (module *GranulatorModule) DSP(timestamp int64) {
+	buflen := module.BufferLength
+
+	outBuffer1 := module.Outlets[0].Buffer
+	outBuffer2 := outBuffer1
+
+	if len(module.Outlets) == 2 {
+		outBuffer2 = module.Outlets[1].Buffer
+	}
+
+	for i := int32(0); i < buflen; i++ {
+		left, right := module.granulator.Process(timestamp, module.GetSampleRate())
+
+		outBuffer1[i] = left
+		outBuffer2[i] = right
+	}
+}
+
+/*
+	Test grain interfaces
+*/
+
+type TestGenerator struct {
+}
+
+func (generator *TestGenerator) GenerateTick(timestamp int64) bool {
+	return rand.Float64() > 0.95
+}
+
+func (generator *TestGenerator) GenerateDuration(timestamp int64) float64 {
+	return (rand.Float64()*100 + 5) / 1000.0
+}
+
+func (generator *TestGenerator) GenerateParameters(timestamp int64) interface{} {
+	parameters := make(map[string]interface{})
+	parameters["frequency"] = rand.Float64()*2800.0 + 100.0
+	parameters["amplitude"] = rand.Float64()*0.8 + 0.2
+	return parameters
+}
+
+func (generator *TestGenerator) GetGrain() Grain {
+	return NewTestGrain()
+}
+
+func (generator *TestGenerator) NumChannels() int {
+	return 1
+}
+
+type TestGrain struct {
+	level float64
+	slope float64
+	curve float64
+	osc   *components.Osc
+}
+
+func NewTestGrain() *TestGrain {
+	testGrain := new(TestGrain)
+	testGrain.osc = components.NewOsc(farsounds.SineTable, 0.0, 0.0, 1.0)
+	return testGrain
+}
+
+func (grain *TestGrain) processEnvelope() float64 {
+	level := grain.level
+	grain.level += grain.slope
+	grain.slope += grain.curve
+	return level
+}
+
+func (grain *TestGrain) setupEnvelope(duration int64, amp float64) {
+	rdur := 1.0 / float64(duration)
+	rdur2 := rdur * rdur
+	grain.level = 0.0
+	grain.slope = 4.0 * amp * (rdur - rdur2)
+	grain.curve = -8.0 * amp * rdur2
+}
+
+func (grain *TestGrain) Initialize(duration float64, sr float64, settings interface{}) {
+	numSamps := int64(duration * sr)
+
+	settingsMap := settings.(map[string]interface{})
+	freq := settingsMap["frequency"].(float64)
+	amp := settingsMap["amplitude"].(float64)
+
+	grain.setupEnvelope(numSamps, amp)
+	grain.osc.Inc = freq / sr
+	grain.osc.Phase = 0.0
+}
+
+func (grain *TestGrain) Process() (float64, float64) {
+	out := grain.osc.Process(0) * grain.processEnvelope()
+	return out, out
+}
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/almerlucke/go-farsounds/examples"
 	"github.com/almerlucke/go-farsounds/farsounds"
 
 	// make sure standard components are loaded
 	_ "github.com/almerlucke/go-farsounds/farsounds/components"
+
+	"github.com/almerlucke/go-farsounds/farsounds/components/granulator"
 )
 
 func setup() {
@@ -17,13 +17,24 @@ func setup() {
 func main() {
 	setup()
 
-	inputJSONFile := "examples/exampleScripts/patchvoice/mainPatch.json"
-	outputSoundFile := "/users/almerlucke/Desktop/sinvoice"
+	/*
+		inputJSONFile := "examples/exampleScripts/patchvoice/mainPatch.json"
+		outputSoundFile := "/users/almerlucke/Desktop/sinvoice"
 
-	fmt.Printf("Generate soundfile from patch %v\n", inputJSONFile)
+		fmt.Printf("Generate soundfile from patch %v\n", inputJSONFile)
 
-	err := farsounds.SoundFileFromScript(inputJSONFile, outputSoundFile, 20.0)
-	if err != nil {
-		fmt.Printf("err %v\n", err)
-	}
+		err := farsounds.SoundFileFromScript(inputJSONFile, outputSoundFile, 20.0)
+		if err != nil {
+			fmt.Printf("err %v\n", err)
+		}
+	*/
+
+	testGenerator := &granulator.TestGenerator{}
+	gr := granulator.NewGranulator(testGenerator, testGenerator, testGenerator, testGenerator)
+	grModule := granulator.NewGranulatorModule(gr, 512, 44100.0)
+	patch := farsounds.NewPatch(0, 1, 512, 44100.0)
+	patch.Modules.PushBack(grModule)
+	grModule.Connect(0, patch.OutletModules[0], 0)
+
+	farsounds.SoundFileFromPatch(patch, "/Users/almerlucke/Desktop/grains", 10.0)
 }
